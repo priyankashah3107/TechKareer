64371d6544fba42f7386b4c6f126d8ada54220dc
Priyanka <priyankashah3107@gmail.com>
2024-06-25 12:58:48 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import { User } from 'lucide-react'
+import { MessageSquare } from 'lucide-react'
+import { Eye } from 'lucide-react'
+import { BellIcon } from 'lucide-react'
+import { LocateIcon } from 'lucide-react'
+import { PinIcon } from 'lucide-react'
+import { DatabaseIcon } from 'lucide-react'
+import { DollarSign } from 'lucide-react'
+import { BadgeDollarSign } from 'lucide-react'
+import { DollarSignIcon } from 'lucide-react'
+import { DatabaseZapIcon } from 'lucide-react'
+import { DatabaseBackupIcon } from 'lucide-react'
+import { Landmark } from 'lucide-react'
+import { HandCoins } from 'lucide-react'
+import { View } from 'lucide-react'
+import { MessageCircleReply } from 'lucide-react'
+import { Users } from 'lucide-react'
+import { HomeIcon } from 'lucide-react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+     <div>
+      <Users />
+      <User />
+      <MessageSquare />
+      <Eye />
+      <HandCoins />
+      <BellIcon />
+      {/* <LocateIcon />
+      <Landmark /> */}
+      <PinIcon />
+      <DatabaseIcon />
+       <DollarSign />
+     
+     
+      
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/comonents/AboutJob.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutJob = () => {
+  return (
+    <div>AboutJob</div>
+  )
+}
+
+export default AboutJob
